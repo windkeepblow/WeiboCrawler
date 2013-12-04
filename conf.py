b6572c60b7b61a09f68bb235102e173280ba2c89
shaoyf <shaoyf2011@gmail.com>
2013-12-04 22:55:28 +0800

    add relogin

--- conf.py
+++ conf.py
@@ -1,10 +1,11 @@
 ##Configration file of weibo crawler
 
 database = "weibodb" #The name of the database
-waitTime = 100 #When reach the request limit, how many seconds will the program wait for
+waitTime = 300 #When reach the request limit, how many seconds will the program wait for
 logPath = "WeiboCrawler.log"
 startID = "1197161814" #From which user to begin crawling, "kaifulee"
 maxWeiboPage = 10 #Only to crawl the recent 10 pages(num: 460) weibo of the user
+relogin = 100 #After how many iterations for the crawler to relogin
 
 username = "shaoyf2013@163.com"  #user name of weibo to login
 pwd = "aa123456" #password of weibo to login

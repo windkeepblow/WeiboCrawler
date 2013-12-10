b377f7f8cf47e423639aab3eed7da9838e8602fb
shaoyf <shaoyf2011@gmail.com>
2013-12-10 22:01:49 +0800

    wait time: 600

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 ##Configration file of weibo crawler
 
 database = "weibodb" #The name of the database
-waitTime = 300 #When reach the request limit, how many seconds will the program wait for
+waitTime = 600 #When reach the request limit, how many seconds will the program wait for
 logPath = "WeiboCrawler.log"
 startID = "1197161814" #From which user to begin crawling, "kaifulee"
 maxWeiboPage = 10 #Only to crawl the recent 10 pages(num: 460) weibo of the user
